ed20712b3056a6a2e5859e9da78ca768d488152e
Gabriel <rinaldinigabriel3@gmail.com>
2023-06-29 15:51:49 -0300

    Conta Bancaria

---
+++ Conta.java
@@ -0,0 +1,70 @@
+
+public abstract class Conta {
+
+	protected double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	private static int total = 0;
+
+	public Conta(int agencia, int numero) {
+		Conta.total++;
+//		System.out.println("o total de contas é " + Conta.total);
+		this.agencia = agencia;
+		this.numero = numero;
+		// this.saldo = 100;
+//		System.out.println("estou criando uma contam " + this.numero);
+	}
+
+	public abstract void deposita(double valor);
+
+	public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean transfere(double valor, Conta destino) {
+		if (this.saca(valor)) {
+			destino.deposita(valor);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public double getSaldo() {
+		return this.saldo;
+	}
+
+	public int getNumero() {
+		return this.numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getAgencia() {
+		return this.agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+
+	public Cliente getTitular() {
+		return titular;
+	}
+
+	public static int getTotal() {
+		return Conta.total;
+	}
+}
